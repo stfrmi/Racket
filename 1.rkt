30dcc6d6784a1ac854c813c30607e176562ecd12
stfrmi <stfrmi09@mail.ru>
2025-09-06 19:42:36 +0300

    The square root
    Calculation of the approximation to the square root of the number x as the limit of a sequence."

---
+++ 1.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+(define epsilon 0.001)
+
+;считает квадрат числа;
+(define (square x) (* x x))
+
+;достаточно ли близки два числа x и y по модулю разницы, с учетом заданной точности epsilon;
+(define (close-enough? x y) (< (abs (- x y)) epsilon))
+
+;возвращает среднее значение x и y;
+(define (average x y) (/ (+ x y) 2))
+
+(define (improve y x) (/ (+ y (/ x y)) 2))
+
+(define (sqrt-loop y x) (if (close-enough? (square y) x) y (sqrt-loop (improve y x) x)))
+
+(define (sqrt x) (sqrt-loop 1.0 x))
+
+(displayln (sqrt 5))
+
+
+
+
